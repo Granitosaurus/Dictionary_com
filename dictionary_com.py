3e66f83c1903931c0dd6489507463b83bce45a5d
granitas <bernardas.alisauskas@gmail.com>
2014-07-18 15:30:02 +0300

    Added proper except catching, fixed docstrings, changed double underscore args to single underscore

--- dictionary_com.py
+++ dictionary_com.py
@@ -1,32 +1,39 @@
 import re
-
-__author__ = 'Bernard @ bernardas.alisauskas@gmail.com'
 import requests
 import bs4
+__author__ = 'Bernard @ bernardas.alisauskas@gmail.com'
+
+"""
+Dictionary.com scraper/API package for Python, see https://github.com/Granitas/Dictionary_com for more details.
+Copyright - feel free to do whatever you like.
+"""
 
 
 class DictionaryReference:
-    """Creates http://dictionary.reference.com/ object which can be used to retrieve definitions and audio pronunciations
+    """
+    Creates http://dictionary.reference.com/ object which can be used to retrieve definitions and audio pronunciations
     of a passed word.
-    args:
-    word - word of which definitions will be retrieved"""
+
+    Keyword Arguments:
+    word -- word of which definitions will be retrieved
+    """
 
     def __init__(self, word):
         self.word = word
         self.definition_list = []
         self.extra_list = []
-        self.__request = None
-        self.__soup = None
-        self.__get_page_contents()
+        self._request = None
+        self._soup = None
+        self._get_page_contents()
 
-    def __get_page_contents(self):
+    def _get_page_contents(self):
         """Retrieve dictionary page contents, runs automatically upon creation of an object"""
-        self.__request = requests.get('http://dictionary.reference.com/browse/' + self.word)
-        self.__soup = bs4.BeautifulSoup(self.__request.text)
+        self._request = requests.get('http://dictionary.reference.com/browse/' + self.word)
+        self._soup = bs4.BeautifulSoup(self._request.text)
 
     def get_definitions(self):
         """Finds definitions in the page soup and appends them to self.definition_list"""
-        tables = self.__soup.find_all('div', class_="KonaBody")
+        tables = self._soup.find_all('div', class_="KonaBody")
         for table in tables:
             if table['style'] == '':
                 # print(table)
@@ -44,42 +51,53 @@ class DictionaryReference:
             self.definition_list.append(Definition(self.word, word_type, text, origin))
 
     def save_html(self, file_name=None):
-        """Saves the html of a definition"""
+        """
+        Saves the html of a definition
+
+        Keyword arguments:
+        file_name -- name of a saved file, include .html suffix (default self.word + .html)
+        """
         if file_name is None:
             file_name = '{word}.html'.format(word=self.word)
         with open(file_name, 'w') as html_file:
             print("Writing to {0}.html".format(self.word))
-            html_file.write(self.__soup.prettify())
+            html_file.write(self._soup.prettify())
 
     def get_html(self):
         """Returns string html of a definition"""
-        return self.__soup.prettify()
+        return self._soup.prettify()
 
     def save_to_mp3(self, file_name=None):
-        """Saves mp3 pronunciation sound file
-        file_name - name of the file, must include .mp3 suffix; Default = self.word + '.mp3'
+        """
+        Saves mp3 pronunciation sound file.
+
+        Keyword Arguments:
+        file_name -- name of the file, must include .mp3 suffix (default self.word + '.mp3')
         """
         if file_name is None:
             file_name = self.word + '.mp3'
         try:
             with open(file_name, 'wb') as mp3_file:
-                file_url = self.__soup.find('a', class_='audspk')['href']
+                file_url = self._soup.find('a', class_='audspk')['href']
                 mp3_file.write(requests.get(file_url).content)
                 print("writing to: {0}".format(file_name))
-        except:
-            print("Failed to save mp3 file")
+        except (FileNotFoundError, PermissionError) as e:
+            print("Failed to save mp3 file: {error}".format(error=e))
 
     def __str__(self):
         return "The Dictionary_com object for word: {}".format(self.word)
 
 
 class Definition:
-    """Definition object, contains :
-    word - word that is being defned
-    type - word type (i.e. noun)
-    definition - word definition
-    origin - word origin
-    of a word that is being defined"""
+    """
+    Definition object, used to store definitions retrieved by DictionaryReference object.
+
+    Keyword arguments:
+    word -- word that is being defined
+    type -- word type (i.e. noun)
+    definition -- word definition
+    origin -- word origin
+    """
     def __init__(self, word, word_type, definition, origin):
         self.word = word
         self.type = word_type
@@ -99,7 +117,7 @@ def main():
     dr.get_definitions()
     for df in dr.definition_list:
         print(df)
-    print('html:\n{}'.format(dr.get_html()))
+    print('html:\n{separator}\n{html}\n{separator}'.format(html=dr.get_html(), separator='-'.center(79, '-')))
 
 if __name__ == '__main__':
     main()
